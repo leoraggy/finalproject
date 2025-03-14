d27231239e982f1194b8c5d3fc30f141a4b5708c
leoraggy <leoragual35@gmail.com>
2025-03-14 11:39:56 -0700

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,37 @@
-export default function page() {
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export default async function page() {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  const accounts = await prisma.account.findMany();
   return (
     <div>
-      <h1>Account Page</h1>
+      <h1>This is the account page.</h1>
+      {accounts.map((account) => (
+        <div key={account.account_id}>
+          <Link
+            className={buttonVariants({ variant: "outline" })}
+            key={account.account_id}
+            href={`/account/${account.account_id}`}
+          >
+            {account.account_name} {account.account_type}{" "}
+            {account.balance.toString()}
+          </Link>
+        </div>
+      ))}
+      <Link
+        className={buttonVariants({ variant: "outline" })}
+        href="/account/create"
+      >
+        Open a new account
+      </Link>
     </div>
   );
 }
