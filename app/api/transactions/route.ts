16f8f948a1c0d31182a527ae0f36c666e10d2ea0
leoraggy <leoragual35@gmail.com>
2025-03-13 22:37:11 -0700

    Fixed routes

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -1,12 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
     const transactions = await prisma.transaction.findMany();
     return NextResponse.json(transactions);
-  } catch (error: any) {
-    // Return error message
+  } catch (error: unknown) {
+    // Type guard to ensure error is an instance of Error
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
     return NextResponse.json(
       { error: "Failed to fetch transactions" },
       { status: 500 }
@@ -24,8 +27,14 @@ export async function POST(request: NextRequest) {
       data: body,
     });
     return NextResponse.json(newTransactions, { status: 201 });
-  } catch (error: any) {
-    console.error("Error creating transaction:", error);
-    return NextResponse.json({ message: error.message }, { status: 400 });
+  } catch (error: unknown) {
+    // Type guard to ensure error is an instance of Error
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+    return NextResponse.json(
+      { message: "Unknown error occurred" },
+      { status: 400 }
+    );
   }
 }
